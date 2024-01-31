86153e446169c19efda8d7795f89d42201580de5
Sausheelya Tripathi <sausheelya42@gmail.com>
2024-01-31 13:36:54 +0530

    Added test cases for all the direction for move forward method in spec

--- app/controllers/spacecraft_movements_controller.rb
+++ app/controllers/spacecraft_movements_controller.rb
@@ -8,5 +8,22 @@ private
     @position = { x: 0, y: 0, z: 0 }
     @direction = 'N'
   end
+
+  def move_forward
+    case @direction
+    when 'N'
+      @position[:y] += 1
+    when 'S'
+      @position[:y] -= 1
+    when 'E'
+      @position[:x] += 1
+    when 'W'
+      @position[:x] -= 1
+    when 'Up'
+      @position[:z] -= 1
+    when 'Down'
+      @position[:z] += 1
+    end
+  end
 end
 
--- spec/requests/spacecraft_movements_spec.rb
+++ spec/requests/spacecraft_movements_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe SpacecraftMovementsController do
   let(:controller) { SpacecraftMovementsController.new }
 
@@ -11,13 +12,46 @@ RSpec.describe SpacecraftMovementsController do
   end
 
   describe '#move_forward' do
-    it 'moves forward in the correct direction' do
+    it 'moves forward in the correct direction (North)' do
       controller.send(:set_initial_state)
+      controller.instance_variable_set(:@direction, 'N')
       controller.send(:move_forward)
       expect(controller.instance_variable_get(:@position)).to eq({ x: 0, y: 1, z: 0 })
     end
+
+    it 'moves forward in the correct direction (South)' do
+      controller.send(:set_initial_state)
+      controller.instance_variable_set(:@direction, 'S')
+      controller.send(:move_forward)
+      expect(controller.instance_variable_get(:@position)).to eq({ x: 0, y: -1, z: 0 })
+    end
+
+    it 'moves forward in the correct direction (East)' do
+      controller.send(:set_initial_state)
+      controller.instance_variable_set(:@direction, 'E')
+      controller.send(:move_forward)
+      expect(controller.instance_variable_get(:@position)).to eq({ x: 1, y: 0, z: 0 })
+    end
+
+    it 'moves forward in the correct direction (West)' do
+      controller.send(:set_initial_state)
+      controller.instance_variable_set(:@direction, 'W')
+      controller.send(:move_forward)
+      expect(controller.instance_variable_get(:@position)).to eq({ x: -1, y: 0, z: 0 })
+    end
+
+    it 'moves forward in the correct direction (Up)' do
+      controller.send(:set_initial_state)
+      controller.instance_variable_set(:@direction, 'Up')
+      controller.send(:move_forward)
+      expect(controller.instance_variable_get(:@position)).to eq({ x: 0, y: 0, z: -1 })
+    end
+
+    it 'moves forward in the correct direction (Down)' do
+      controller.send(:set_initial_state)
+      controller.instance_variable_set(:@direction, 'Down')
+      controller.send(:move_forward)
+      expect(controller.instance_variable_get(:@position)).to eq({ x: 0, y: 0, z: 1 })
+    end
   end
-
 end
-
-
