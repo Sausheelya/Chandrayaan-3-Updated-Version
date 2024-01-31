0af63acbb0546d98ba3a00ab036c88197f764a4e
Sausheelya Tripathi <sausheelya42@gmail.com>
2024-01-31 13:29:47 +0530

    Extend test coverage by introducing test cases for the move_forward method in SpacecraftMovementsController. Ensure correct movement in different initial directions, covering the scenarios of 'N', 'S', 'E', 'W', 'Up', and 'Down'.

--- spec/requests/spacecraft_movements_spec.rb
+++ spec/requests/spacecraft_movements_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe SpacecraftMovementsController do
       expect(controller.instance_variable_get(:@direction)).to eq('N')
     end
   end
+
+  describe '#move_forward' do
+    it 'moves forward in the correct direction' do
+      controller.send(:set_initial_state)
+      controller.send(:move_forward)
+      expect(controller.instance_variable_get(:@position)).to eq({ x: 0, y: 1, z: 0 })
+    end
+  end
+
 end
 
 
